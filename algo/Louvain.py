dd9faa605d37d730b4b8e1a4382646b71350c944
Qingquan Bao <Faust-bqq@sjtu.edu.cn>
2021-11-24 22:05:26 +0800

    version1.0

--- algo/Louvain.py
+++ algo/Louvain.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python'3'
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 22 11:14:09 2021
+Created on Mon Nov 22 11:1'4':09 2021
 
 @author: mac
 """
 import networkx as nx
+import pickle
 import pandas as pd
 from Louvain_model import DirectedGraph 
+from collections import Counter, defaultdict
 
 def read_csv(file_name):
     G = nx.DiGraph()
@@ -19,7 +21,79 @@ def read_csv(file_name):
             G.add_edges_from(edge)
     return G
 
+def read_label(file_name):
+    label_list = defaultdict(lambda: 'UNK')
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if i == 0 : continue
+            id, label = line.replace('\n', '').split(',')
+            label_list[id] = label
+    return label_list
+
+def toy():
+     G = nx.DiGraph()
+     G.add_edges_from( 
+         [('1', '2'), ('2','3'), ('3','4'), ('4','1'), ('2','1'), ('3','2'), ('4','3'), ('1','4'),
+        ('4','9'), ('9','5') , ('5','6'), ('9','4'), ('5','9'), ('6','5'),
+        ('6','7'), ('7','8') , ('8','6')]
+     )
+     return G
+
+def voting(res, gt):
+    voting_list = {i:Counter() for i in res.keys()}
+    for name, community in res.items():
+        for node in community:
+            voting_list[name].update([gt[node]])
+        voting_list[name].pop('UNK')
+    
+    final_ = { class_id:[] for class_id in gt.values()}
+    for key in voting_list:
+        sort = sorted(voting_list[key], key=voting_list[key].get, reverse=True)
+        final_[sort[0]] += res[key]
+        print(key, '\t', sort)
+
+    return final_
+
+def eval(res, gt):
+    correct = 0.
+    for name, community in res.items(): 
+        for node in community:
+            correct += 1 if gt[node] == name else 0
+
+    return correct / 300 
+
+def to_csv(res, output_file):
+    with open (output_file, 'w') as f:
+        f.write('id,category\n')
+        for name, community in res.items():
+            for node in community:
+                f.write(node + ',' + name + '\n')
+
 if __name__ == '__main__':
-    G = read_csv('edges_update.csv')
+    G = read_csv('dataset/edges_update.csv')
+    #G = toy()
     DG = DirectedGraph(G)
-    DG.louvain()
+    res = DG.louvain(9, 9)
+
+    gt = read_label('dataset/ground_truth.csv')
+    print(len(gt))
+    voting_res = voting(res, gt)
+    print('1st acc= ', eval(voting_res, gt))
+
+    # Next is some trick, cuz we find that class 0 and class 1 are not well seperated
+    # So we do clustering again.
+    for commu in [ '2', '3', '4']:
+        G.remove_nodes_from(voting_res[commu])
+    new_DG = DirectedGraph(G)
+    new_res = new_DG.louvain(2, 10)
+    voting2 = voting(new_res, gt)
+
+    final_res = {'0' : voting2['0'], '1': voting2['1'], 
+                 '2' : voting_res['2'], '3': voting_res['3'], '4': voting_res['4'] }
+
+    with open("trial3.txt", "wb") as fp:   #Pickling
+        pickle.dump(final_res, fp)
+
+    to_csv(final_res, 'trial3.csv')
+    print('2nd acc= ', eval(final_res,gt))
--- algo/Louvain_model.py
+++ algo/Louvain_model.py
@@ -5,14 +5,7 @@ Created on Mon Nov 22 11:14:09 2021
 
 @author: mac
 """
-
-'''
-Node transfer e.g. Node 2  -> Node 9  and Node 9 -> Node1. Then remove Node 9 ?
-'''
-
 from collections import Counter
-
-from networkx.generators import community
 from tqdm import tqdm
 
 
@@ -22,10 +15,12 @@ class CommunitySuperNode:
         self.node_list = []
         self.community = []
 
-        self.in_neighbors = []
-        self.in_weight = []
-        self.out_neighbors = []
-        self.out_weight = []
+        self.in_neighbors = Counter()
+        self.out_neighbors = Counter()
+        #self.in_neighbors = []
+        #self.in_weight = []
+        #self.out_neighbors = []
+        #self.out_weight = []
 
         self.out_total_degree = 0
         self.in_total_degree = 0
@@ -39,78 +34,89 @@ class CommunitySuperNode:
         Input:  
             edges: (list) list of the outward neighbors
         '''
+        self.node_list += [self.name]
         self.community.append(name)
-        self.in_neighbors += [e[0] for e in in_edges]
-        self.in_weight = [1 for i in range(len(self.in_neighbors))]
-        self.out_neighbors += edges
-        self.out_weight = [1 for i in range(len(self.out_neighbors))]
+        self.in_neighbors.update({e: 1 for e in in_edges})
+        self.out_neighbors.update({e: 1 for e in edges})
+        #self.in_neighbors += [e[0] for e in in_edges]
+        #self.in_weight = [1 for i in range(len(self.in_neighbors))]
+        #self.out_neighbors += edges
+        #self.out_weight = [1 for i in range(len(self.out_neighbors))]
         
         self.in_total_degree = len(self.in_neighbors)
         self.out_total_degree = len(self.out_neighbors)
         self.in_d = self.in_total_degree
         self.out_d = self.out_total_degree
 
-    def cal_delta_modu(self, m, nodex):
+    def cal_delta_modu(self, m, nodex, graph):
         assert isinstance(nodex, CommunitySuperNode)
         if len(self.community) == 0:
             return -1000000
         else:
-            k_i_in  = set(nodex.out_neighbors)  &  set(self.community)
-            k_i_out = set(nodex.in_neighbors) &  set(self.community)
-            d_i_in = sum([ nodex.out_weight[nodex.out_neighbors.index(n)] for n in k_i_in ])
-            d_i_out = sum([ nodex.in_weight[nodex.in_neighbors.index(n)] for n in k_i_out ])
+            #k_i_in  = set(nodex.out_neighbors)  &  set(self.community)
+            #k_i_out = set(nodex.in_neighbors) &  set(self.community)
+            #d_i_in = sum([ nodex.out_weight[nodex.out_neighbors.index(n)] for n in k_i_in ])
+            #d_i_out = sum([ nodex.in_weight[nodex.in_neighbors.index(n)] for n in k_i_out ])
+            d_i_in = sum( [nodex.out_neighbors[key] if  graph.community_list[key].to_commu == self.name else 0 \
+                           for key in nodex.out_neighbors.keys() ] )
+            d_i_out = sum( [nodex.in_neighbors[key] if  graph.community_list[key].to_commu == self.name else 0 \
+                           for key in nodex.in_neighbors.keys() ] )
 
             modu = (d_i_in + d_i_out ) / m 
-            modu -= (nodex.out_d * self.in_total_degree + nodex.in_d * self.out_total_degree ) / m**2
+            modu -= (nodex.out_d * self.in_total_degree + nodex.in_d * self.out_total_degree ) / (m**2)
             return modu
 
-    def remove(self, m, nodex ):
+    def remove(self, m, nodex, graph ):
         self.community.remove(nodex.name)
+        self.out_total_degree -= nodex.out_d
+        self.in_total_degree  -= nodex.in_d
         if len(self.community) == 0:
             return 0
         else:
-            self.out_total_degree -= nodex.out_d
-            self.in_total_degree  -= nodex.in_d
-            return self.cal_delta_modu(m, nodex)
+            return self.cal_delta_modu(m, nodex, graph)
             #self.internals -= 
 
     def append(self, nodex):
         # only use for phase1, so we do not need to append node_list 
         self.community.append(nodex.name)
-        if self.name == nodex.name: return
-
         self.out_total_degree += nodex.out_d
         self.in_total_degree  += nodex.in_d
 
     def merge(self, graph):
         assert len(self.community) > 0, 'Oooops, len of community = {} , seems to be wrong'.format(len(self.community))
+        self.new_node_list = []
         for node in self.community:
-            if node == self.name : continue
             member = graph.community_list[node]
-            self.node_list += member.node_list
-            self.in_neighbors += member.in_neighbors
-            self.out_neighbors += member.out_neighbors
-            self.in_weight += member.in_weight
-            self.out_weight += member.out_weight
+            self.new_node_list += member.node_list
+
+            self.in_neighbors.update(member.in_neighbors)
+            self.out_neighbors.update(member.out_neighbors)
 
         self.name =self.community[0]
         community_ = {old: self.name for old in self.community}
         self.community = [self.name]
+        self.to_commu = self.name
+
         return community_
 
     def update(self, old2new):
         new_in_neighbors  = Counter()
         new_out_neighbors = Counter()
-        for neigh, weight in zip(self.in_neighbors, self.in_weight):
+
+        for neigh, weight in self.in_neighbors.items():
             new_in_neighbors.update( {old2new[neigh]: weight})
-        for neigh, weight in zip(self.out_neighbors, self.out_weight):
+        for neigh, weight in self.out_neighbors.items():
             new_out_neighbors.update({old2new[neigh]: weight})
-        
-        self.in_neighbors = list(new_in_neighbors.keys())
-        self.in_weight    = list(new_in_neighbors.values())
-        self.out_neighbors = list(new_out_neighbors.keys())
-        self.out_weight    = list(new_out_neighbors.values())
+
+        self.in_neighbors = new_in_neighbors
+        self.out_neighbors = new_out_neighbors
+
+        self.in_d =  sum(self.in_neighbors.values())
+        self.out_d = sum(self.out_neighbors.values())
             
+        # To avoid recursive update, We must modify node_list HERE!
+        self.node_list = self.new_node_list
+        self.new_node_list = []
 
 class DirectedGraph:
     def __init__(self, graph):
@@ -127,58 +133,60 @@ class DirectedGraph:
 
         self.m = graph.number_of_edges()
 
-    def louvain(self, community_num=5):
+    def louvain(self, community_num=9, max_iter=30):
         num = 100
         while (num > community_num):
-            self._phase1()
+            self._phase1(max_iter=max_iter)
             num = self._phase2()
-            print(num)
-            break
+            print('Community num: ', num)
+            if (num < 50): max_iter = 1
+            if (num < 10):
+                for name, community in self.community_list.items():
+                    print(name, '\t', len(community.node_list))
+            
+        res_ = {}
+        for name, community in self.community_list.items():
+            res_[name] = community.node_list
+        return res_
 
-
-    def _phase1(self, max_iter=1):
+    def _phase1(self, max_iter=6):
         stop_flag = False
         iter = 0
         # When Modularity converges, stop
         while not stop_flag and iter < max_iter:
             stop_flag = True
             iter += 1
-            print(iter)
             # iterate very node
-            for node in tqdm(self.community_list.values()):
-                # tmp
+            for node in tqdm(self.community_list.values(), desc='Iteration={}'.format(iter)):
                 cur_community = self.community_list[node.to_commu]
-                #if node.name not in node.community: continue
-                # tmp
+
                 gain_M = {}
-                gain_M[cur_community.name] = cur_community.remove(self.m, node)
+                gain_M[cur_community.name] = cur_community.remove(self.m, node, self)
                 # test every gain in Modularity
                 neighbors = node.out_neighbors + node.in_neighbors
                 for neighbor in neighbors:
                     neighbor_commu = self.community_list[neighbor].to_commu
-                    gain_M[neighbor_commu] = self.community_list[neighbor_commu].cal_delta_modu(self.m, node)
+                    gain_M[neighbor_commu] = self.community_list[neighbor_commu].cal_delta_modu(self.m, node, self)
                 
                 best_com = max(gain_M, key=gain_M.get)
                 self.community_list[best_com].append(node)
+                if best_com != node.to_commu: stop_flag = False
                 node.to_commu = best_com
-                if best_com != node.name: stop_flag = False
 
     def _phase2(self):
-        new_community = []
+        new_community = {}
         old2new = {}
         # merge the super node
         for node in self.community_list.values():
             if len(node.community) == 0 :
                 continue
             old2new.update(node.merge(self))
-            new_community.append(node.name)
+            new_community[node.name] = node
 
         # Rewire the edge
-        for node in self.community_list.values():
-            if len(node.community) == 0 :
-                continue 
-            node.update(old2new)
+        for node in new_community:
+            new_community[node].update(old2new)
 
-        new = {key: self.community_list[key] for key in new_community}
-        self.community_list = new
+        #new = {key: self.community_list[key] for key in new_community}
+        self.community_list = new_community
         return len(self.community_list)
